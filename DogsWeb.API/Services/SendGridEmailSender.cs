778b3c56a7889520f7b22b8aad7eac07c38c4f66
raqbiel <raqbiel@gmail.com>
2021-04-06 14:35:12 +0200

    Update SendGridEmailSender.cs

--- DogsWeb.API/Services/SendGridEmailSender.cs
+++ DogsWeb.API/Services/SendGridEmailSender.cs
@@ -15,9 +15,9 @@ namespace DogsWeb.API.Services
         public async Task<SendEmailResponse> SendEmailAsync(string userEmail, string emailSubject, string message) 
         {
             
-            var apiKey = "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";
+            var apiKey = "";
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("raqbiel@gmail.com", "DogsWeb");
+            var from = new EmailAddress("", "DogsWeb");
             var subject = emailSubject;
             var to = new EmailAddress(userEmail, "Test");
             var plainTextContent = message;
@@ -27,36 +27,7 @@ namespace DogsWeb.API.Services
 
             return new SendEmailResponse();
         }
-        // public async Task<SendEmailResponse> SendEmailAsyncs(string userEmail, string emailSubject, string message){
-
-        //     var sendGridClient = new SendGridClient("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");
-        //     var sendGridMessage = new SendGridMessage();
-        //     sendGridMessage.SetFrom("vfabing@live.com", "Vivien FABING");
-        //     sendGridMessage.AddTo("usertest2019@yopmail.com", "user test");
-        //     sendGridMessage.SetTemplateId("MY_TEMPLATE_ID");
-        //     sendGridMessage.SetTemplateData(new HelloEmail
-        //     {
-        //         Name = "Vivien",
-        //         Url = "https://www.vivienfabing.com"
-        //     });
-
-        //     var response = sendGridClient.SendEmailAsync(sendGridMessage).Result;
-        //     if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
-        //     {
-        //         Console.WriteLine("Email sent");
-        //     }
-
-        //      return new SendEmailResponse();
-        // }
-        //     private class HelloEmail
-        // {
-        //     [JsonProperty("name")]
-        //     public string Name { get; set; }
-
-        //     [JsonProperty("url")]
-        //     public string Url { get; set; }
-        // }
-
+       
         
     }
 }
